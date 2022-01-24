f2dd4c9a2a1acc8bbb5c7f56bd3fd4031fce9d34
Diego Patiño <diego_Oescobar@outlook.com>
2022-01-24 10:08:34 -0500

    Prueba

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,93 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Customer customers = new Customer() { first_name = "Juan" };
+            Customer customers = new Customer() {
+                store_id = 1,
+                first_name = "Diego",
+                last_name = "Pati;o",
+                email = "diego@gmail.com",
+                activateboll = true,
+                create_date = new DateTime(2021, 01, 24),
+                last_update = new DateTime(2021, 01, 24),
+                activate = true
+
+            };
+
+            Customer customers2 = new Customer()
+            {
+                store_id = 1,
+                first_name = "Diego",
+                last_name = "Pati;o",
+                email = "diego@gmail.com",
+                activateboll = true,
+                create_date = new DateTime(2021, 01, 24),
+                last_update = new DateTime(2021, 01, 24),
+                activate = true
+
+            };
+
+            Customer customers1 = new Customer()
+            {
+                store_id = 1,
+                first_name = "Diego",
+                last_name = "Pati;o",
+                email = "diego@gmail.com",
+                activateboll = true,
+                create_date = new DateTime(2021, 01, 24),
+                last_update = new DateTime(2021, 01, 24),
+                activate = true
+
+            };
+            Customer customers4 = new Customer()
+            {
+                store_id = 1,
+                first_name = "Diego",
+                last_name = "Pati;o",
+                email = "diego@gmail.com",
+                activateboll = true,
+                create_date = new DateTime(2021, 01, 24),
+                last_update = new DateTime(2021, 01, 24),
+                activate = true
+
+            };
+
+            Payment payment1 = new Payment()
+            {
+                staff_id = 2,
+                amount = 250,
+                payment_date = new DateTime(2021, 01, 24)
+
+            };
+            Payment payment1 = new Payment()
+            {
+                staff_id = 2,
+                amount = 250,
+                payment_date = new DateTime(2021, 01, 24)
+
+            };
+            Payment payment2 = new Payment()
+            {
+                staff_id = 2,
+                amount = 250,
+                payment_date = new DateTime(2021, 01, 24)
+
+            };
+            Payment payment3 = new Payment()
+            {
+                staff_id = 2,
+                amount = 250,
+                payment_date = new DateTime(2021, 01, 24)
+
+            };
+            Payment payment4 = new Payment()
+            {
+                staff_id = 2,
+                amount = 250,
+                payment_date = new DateTime(2021, 01, 24)
+
+            };
+
+
 
         }
     }
